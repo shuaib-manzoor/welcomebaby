5fce0392ea08a044746660b80d9dc95731fa3559
shuaib-manzoor <127669231+shuaib-manzoor@users.noreply.github.com>
2023-07-11 10:26:54 +0530

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,80 +1,109 @@
 /*!
-* Start Bootstrap - Stylish Portfolio v6.0.6 (https://startbootstrap.com/theme/stylish-portfolio)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-stylish-portfolio/blob/master/LICENSE)
-*/
-window.addEventListener('DOMContentLoaded', event => {
+ * Start Bootstrap - Stylish Portfolio v6.0.6 (https://startbootstrap.com/theme/stylish-portfolio)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-stylish-portfolio/blob/master/LICENSE)
+ */
+window.addEventListener("DOMContentLoaded", (event) => {
+  const sidebarWrapper = document.getElementById("sidebar-wrapper");
+  let scrollToTopVisible = false;
+  // Closes the sidebar menu
+  const menuToggle = document.body.querySelector(".menu-toggle");
+  menuToggle.addEventListener("click", (event) => {
+    event.preventDefault();
+    sidebarWrapper.classList.toggle("active");
+    _toggleMenuIcon();
+    menuToggle.classList.toggle("active");
+  });
 
-    const sidebarWrapper = document.getElementById('sidebar-wrapper');
-    let scrollToTopVisible = false;
-    // Closes the sidebar menu
-    const menuToggle = document.body.querySelector('.menu-toggle');
-    menuToggle.addEventListener('click', event => {
-        event.preventDefault();
-        sidebarWrapper.classList.toggle('active');
-        _toggleMenuIcon();
-        menuToggle.classList.toggle('active');
-    })
-
-    // Closes responsive menu when a scroll trigger link is clicked
-    var scrollTriggerList = [].slice.call(document.querySelectorAll('#sidebar-wrapper .js-scroll-trigger'));
-    scrollTriggerList.map(scrollTrigger => {
-        scrollTrigger.addEventListener('click', () => {
-            sidebarWrapper.classList.remove('active');
-            menuToggle.classList.remove('active');
-            _toggleMenuIcon();
-        })
+  // Closes responsive menu when a scroll trigger link is clicked
+  var scrollTriggerList = [].slice.call(
+    document.querySelectorAll("#sidebar-wrapper .js-scroll-trigger")
+  );
+  scrollTriggerList.map((scrollTrigger) => {
+    scrollTrigger.addEventListener("click", () => {
+      sidebarWrapper.classList.remove("active");
+      menuToggle.classList.remove("active");
+      _toggleMenuIcon();
     });
+  });
 
-    function _toggleMenuIcon() {
-        const menuToggleBars = document.body.querySelector('.menu-toggle > .fa-bars');
-        const menuToggleTimes = document.body.querySelector('.menu-toggle > .fa-xmark');
-        if (menuToggleBars) {
-            menuToggleBars.classList.remove('fa-bars');
-            menuToggleBars.classList.add('fa-xmark');
-        }
-        if (menuToggleTimes) {
-            menuToggleTimes.classList.remove('fa-xmark');
-            menuToggleTimes.classList.add('fa-bars');
-        }
+  function _toggleMenuIcon() {
+    const menuToggleBars = document.body.querySelector(
+      ".menu-toggle > .fa-bars"
+    );
+    const menuToggleTimes = document.body.querySelector(
+      ".menu-toggle > .fa-xmark"
+    );
+    if (menuToggleBars) {
+      menuToggleBars.classList.remove("fa-bars");
+      menuToggleBars.classList.add("fa-xmark");
     }
+    if (menuToggleTimes) {
+      menuToggleTimes.classList.remove("fa-xmark");
+      menuToggleTimes.classList.add("fa-bars");
+    }
+  }
 
-    // Scroll to top button appear
-    document.addEventListener('scroll', () => {
-        const scrollToTop = document.body.querySelector('.scroll-to-top');
-        if (document.documentElement.scrollTop > 100) {
-            if (!scrollToTopVisible) {
-                fadeIn(scrollToTop);
-                scrollToTopVisible = true;
-            }
-        } else {
-            if (scrollToTopVisible) {
-                fadeOut(scrollToTop);
-                scrollToTopVisible = false;
-            }
-        }
-    })
-})
+  // Scroll to top button appear
+  document.addEventListener("scroll", () => {
+    const scrollToTop = document.body.querySelector(".scroll-to-top");
+    if (document.documentElement.scrollTop > 100) {
+      if (!scrollToTopVisible) {
+        fadeIn(scrollToTop);
+        scrollToTopVisible = true;
+      }
+    } else {
+      if (scrollToTopVisible) {
+        fadeOut(scrollToTop);
+        scrollToTopVisible = false;
+      }
+    }
+  });
+
+  setInterval(function () {
+    let currentDate = new Date();
+    let dueDate = new Date("Dec 24, 2023 12:00:00");
+    if (dueDate < currentDate) {
+      date2.setDate(dueDate.getDate() + 1);
+    }
+    let difference = dueDate - currentDate;
+    var msec = difference;
+    var hh = Math.floor(msec / 1000 / 60 / 60);
+    var days = hh/24;
+    msec -= hh * 1000 * 60 * 60;
+    var mm = Math.floor(msec / 1000 / 60);
+    msec -= mm * 1000 * 60;
+    var ss = Math.floor(msec / 1000);
+    msec -= ss * 1000;
+    let time = "";
+    if(days > 0){
+        time += "<div>"+Math.floor(days)+" Days</div>";
+    }
+    
+    time += "<div>"+hh%24 + " Hours</div><div>" + mm + " Minutes</div>" +"<div>"+ ss+" Seconds</div>"
+    document.getElementById("countdown").innerHTML = time;
+  }, 1000);
+});
 
 function fadeOut(el) {
-    el.style.opacity = 1;
-    (function fade() {
-        if ((el.style.opacity -= .1) < 0) {
-            el.style.display = "none";
-        } else {
-            requestAnimationFrame(fade);
-        }
-    })();
-};
+  el.style.opacity = 1;
+  (function fade() {
+    if ((el.style.opacity -= 0.1) < 0) {
+      el.style.display = "none";
+    } else {
+      requestAnimationFrame(fade);
+    }
+  })();
+}
 
 function fadeIn(el, display) {
-    el.style.opacity = 0;
-    el.style.display = display || "block";
-    (function fade() {
-        var val = parseFloat(el.style.opacity);
-        if (!((val += .1) > 1)) {
-            el.style.opacity = val;
-            requestAnimationFrame(fade);
-        }
-    })();
-};
+  el.style.opacity = 0;
+  el.style.display = display || "block";
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += 0.1) > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  })();
+}
